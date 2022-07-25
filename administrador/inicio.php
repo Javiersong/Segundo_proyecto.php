fbcbabf6911aec059f53eddb3073c65212d72252
Javiersong <Javizgon123@gmail.com>
2022-07-25 15:57:29 -0600

    Ecommerce

---
+++ administrador/inicio.php
@@ -0,0 +1,15 @@
+<?php include('template/cabeceraa.php'); ?>
+
+
+               <div class="col-md-12">
+               <div class="jumbotron">
+                 <h1 class="display-3">Bienvenido <?php echo $nombreusuario;  ?></h1>
+                <p class="lead">Vamos a administrar todos nuestros libros en el sitio web</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+              <a class="btn btn-primary btn-lg" href="seccion/productos.php" role="button">Administrar libros</a>
+             </p>
+          </div>
+      </div>             
+<?php include('template/piee.php'); ?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("template/cabecera.php"); ?>
+    
+        <div class="jumbotron text-center">
+            <h1 class="display-3">Bienvenidos a Lunita Piercings</h1>
+            <p class="lead">Consulta piercings chidos</p>
+            <hr class="my-2">
+           <img width="400" src="" class="img-thumbnail rounded mx-auto d-block" />
+
+            <p>Más información</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver almacen de piercings</a>
+            </p>
+        </div>
+
+<?php include("template/Pie.php"); ?>
